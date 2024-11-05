5b6cde218414bff8007f609c0c72629dba0f09c3
Sriharsh Dubale <dsriharsh@gmail.com>
2024-11-04 19:55:40 -0500

    seed file updated to add boards and columns

--- seeds/seed_users.js
+++ seeds/seed_users.js
@@ -4,16 +4,66 @@
  */
 export async function seed(knex) {
   await knex("users").del();
-  await knex("users").insert([
+  await knex("boards").del();
+  await knex("columns").del();
+  await knex("cards").del();
+
+  const users = await knex("users")
+    .insert([
+      {
+        name: "Sriharsh Dubale",
+        email: "dsriharsh@gmail.com",
+        password: "password",
+      },
+      {
+        name: "Sri",
+        email: "sriharsh.dubale1@gmail.com",
+        password: "password",
+      },
+    ])
+    .returning("*");
+
+  const boards = await knex("boards")
+    .insert([
+      {
+        user_id: users[0].id,
+        title: "Project 1",
+      },
+      {
+        user_id: users[0].id,
+        title: "Project 2",
+      },
+      {
+        user_id: users[0].id,
+        title: "Project 3",
+      },
+      {
+        user_id: users[0].id,
+        title: "Project 4",
+      },
+    ])
+    .returning("*");
+
+  const columns = await knex("columns").insert([
     {
-      name: "Sriharsh Dubale",
-      email: "dsriharsh@gmail.com",
-      password: "password",
+      board_id: boards[0].id,
+      title: "Todo",
+      position: 1,
     },
     {
-      name: "Sri",
-      email: "sriharsh.dubale1@gmail.com",
-      password: "password",
+      board_id: boards[0].id,
+      title: "Doing",
+      position: 2,
+    },
+    {
+      board_id: boards[0].id,
+      title: "Testing",
+      position: 3,
+    },
+    {
+      board_id: boards[0].id,
+      title: "Done",
+      position: 4,
     },
   ]);
 }
