9b1611992d552d726b3e0767aaa6f6d1ecaa7893
Sriharsh Dubale <dsriharsh@gmail.com>
2024-11-22 19:14:47 -0500

    seed data added for cards

--- seeds/seed_users.js
+++ seeds/seed_users.js
@@ -44,26 +44,59 @@ export async function seed(knex) {
     ])
     .returning("*");
 
-  const columns = await knex("columns").insert([
+  const columns = await knex("columns")
+    .insert([
+      {
+        board_id: boards[0].id,
+        title: "Todo",
+        position: 1,
+      },
+      {
+        board_id: boards[0].id,
+        title: "Doing",
+        position: 2,
+      },
+      {
+        board_id: boards[0].id,
+        title: "Testing",
+        position: 3,
+      },
+      {
+        board_id: boards[0].id,
+        title: "Done",
+        position: 4,
+      },
+    ])
+    .returning("*");
+
+  const cards = await knex("cards").insert([
     {
-      board_id: boards[0].id,
-      title: "Todo",
+      column_id: columns[0].id,
+      title: "Task 1",
       position: 1,
+      description:
+        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum",
     },
     {
-      board_id: boards[0].id,
-      title: "Doing",
+      column_id: columns[0].id,
+      title: "Task 34",
       position: 2,
+      description:
+        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum",
     },
     {
-      board_id: boards[0].id,
-      title: "Testing",
-      position: 3,
+      column_id: columns[1].id,
+      title: "Task 4234",
+      position: 1,
+      description:
+        "Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum",
     },
     {
-      board_id: boards[0].id,
-      title: "Done",
-      position: 4,
+      column_id: columns[2].id,
+      title: "Task 3",
+      position: 1,
+      description:
+        "Latin literature frofessor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum",
     },
   ]);
 }
